9ad47ad49e9a13467ce56714f4c5b8f9ccddbb39
Andrew <andrew.wildwesttech@gmail.com>
2023-02-18 23:22:22 -0500

    emr-admin

--- modules/permission-sets/data.tf
+++ modules/permission-sets/data.tf
@@ -9,3 +9,12 @@ data "aws_identitystore_group" "analysts" {
     attribute_value = "analysts"
   }
 }
+
+#Identify the Group, which we will Assign these Permissions To
+data "aws_identitystore_group" "admins" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+  filter {
+    attribute_path  = "DisplayName"
+    attribute_value = "admins"
+  }
+}
